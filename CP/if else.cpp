396cb4c4ca8375775c8efaf833d28170bfe45aa6
shoaib arif <shoaibarif16@gmail.com>
2023-03-08 22:16:35 +0530

    changess

---
+++ CP/if else.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if ( n % 2 == 0){
+        cout<< "YES";
+    }
+        else if(n % 3 ==0){
+            cout<< "Divisible by 3";
+        }
+
+            else {
+                cout<<" NO "; 
+            }
+
+        
+}
